a5379a9d445456b96fc30cdb0154e34888955b0d
Timofey Shichalin <shichatim@gmail.com>
2022-06-28 22:18:57 -0700

    ensure username , name, password are appropriate

--- data/user.py
+++ data/user.py
@@ -1,9 +1,76 @@
 
+from cmath import inf
 from connect import *
 from config import connection_credential_path, database_name, user_table_name
 
+
+
+# See whether the passed username is valid (can be created).
+# Requires string to be a string (str).
+def validate_string(string, min_length = 0, max_length = inf, extra_legal_chars = None):
+
+    # Null
+    if(string == None):
+        return False
+
+    # String is too short or too long
+    if(len(string) < min_length or len(string) > max_length):
+        return False
+
+
+    # String may only contain alphanumerals and any specified extra characters
+    for i in string:
+        if(not(str.isalnum(i) or i in extra_legal_chars)):
+            return False
+
+    return True
+
+# See whether the passed password is valid (can be created).
+# Allows all characters, so only requirement is length.
+# Requires password to be a string (str).
+def is_password_appropriate(password, min_length = 0, max_length = inf):
+
+    # Null
+    if(password == None):
+        return False
+
+    # String is too short or too long
+    if(len(password) < min_length or len(password) > max_length):
+        return False
+
+    return True
+
+
 # Add a user to the database
+#
+# Return codes:
+# 0  => no error (user was added)
+# -1 => username exists
+# -2 => database error (connecting, accessing table, etc)
+# -3 => username is not appropriate
+# -4 => first or last name illegal
+# -5 => password illegal
 def add_user(username, first_name, last_name, password):
+
+    # Convert to strings for security (and avoid crashes).
+    username = str(username)
+    first_name = str(first_name)
+    last_name = str(last_name)
+    password = str(password)
+
+    # Make sure the user entered an appropriate username
+    if(validate_string(username, min_length=1, max_length=50, extra_legal_chars='_') == False):
+        return -3
+
+    # Make sure the user enetered an appropriate first and last name
+    if(validate_string(first_name, min_length=1, max_length=50, extra_legal_chars='-') == False or
+        validate_string(last_name, min_length=1, max_length=50, extra_legal_chars='-') == False):
+        return -4
+
+    # Make sure the user entered an appropriate password
+    if(is_password_appropriate(password, min_length=4, max_length=50) == False):
+        return -5
+    
     
     # Create the connection.
     credentials = read_credentials_from_file("../" + connection_credential_path)
