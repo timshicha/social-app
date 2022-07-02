85bb6aae647ba6bce5b0c205cf822be55b000c81
Vadim Shicha <shichavadim@gmail.com>
2022-07-01 23:20:07 -0700

    sign in command with ascii

--- sign_up/add_user.php
+++ sign_up/add_user.php
@@ -1,6 +1,31 @@
 <?php
 
-$result = shell_exec("python ../data/user.py add_user " . $_POST["username"] . " " . $_POST["first"] . " " . $_POST["last"] . " -pw:\"". $_POST["password"] . "\"");
+// Converts a string to ascii values seperated with dashes
+// Example: "string" -> "115-116-114-105-110-103"
+function str_to_ascii($str)
+{
+    $ascii_str = "";
+    $len = strlen($str);
+
+    for($i = 0; $i < $len; $i++)
+    {
+        $ascii_str .= strval(ord($str[$i]));
+
+        if($i != $len - 1)
+            $ascii_str .= '-';
+    }
+
+    return $ascii_str;
+}
+
+$username = str_to_ascii($_POST["username"]);
+$first = str_to_ascii($_POST["first"]);
+$last = str_to_ascii($_POST["last"]);
+$password = str_to_ascii($_POST["password"]);
+
+$result = shell_exec("python ../data/user.py add_user {$username} {$first} {$last} {$password}");
+
+
 
 $code = intval($result);
 
