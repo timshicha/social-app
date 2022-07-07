16773d6848a926cb8c90b0c6dbb277716cbffd2f
Timofey Shichalin <shichatim@gmail.com>
2022-07-06 21:28:24 -0700

    create session token when logging in

--- login/login.php
+++ login/login.php
@@ -1,19 +1,23 @@
 <?php
 
+$SUCCESS_CODE = 25;
+
 require __DIR__ . "/../tools.php";
 
 $username = str_to_ascii($_POST["username"]);
 $password = str_to_ascii($_POST["password"]);
 
-$result = shell_exec("python ../data/user.py login_user {$username} {$password}");
+$result = intval(shell_exec("python ../data/user.py login_user {$username} {$password}"));
 
-$code = intval($result);
+$return_data = array($result, "");
 
-if($code == 25)
-    echo "Successfully logged in";
-else if($code == -1)
-    echo "Invalid username";
-else if($code == -2)
-    echo "Invalid password";
+// If logging in ran successfully, start a session for the user and send them the session token
+if($result == $SUCCESS_CODE)
+{
+    session_start();
+    $return_data[1] = session_id(); // Return the session ID back to JS
+}
+
+echo json_encode($return_data)
     
 ?>
