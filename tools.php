a265580366902445448aa66ab7f7935ae6325f15
Timofey Shichalin <shichatim@gmail.com>
2022-07-06 21:38:19 -0700

    ascii str values padded to all be 3 digits

--- tools.php
+++ tools.php
@@ -9,8 +9,22 @@ function str_to_ascii($str)
 
     for($i = 0; $i < $len; $i++)
     {
-        $ascii_str .= strval(ord($str[$i]));
+        // Convert each character to ascii value
+        $to_add = strval(ord($str[$i]));
 
+        // Make sure each ascii value is 3 digits, so this means
+        // we need to pad ascii values less than three digits with
+        // zeros.
+        $pad_zeros = 3 - strlen($to_add); // Count the number of zeros to pad
+
+        // Pad the ascii value with zeros
+        for($j = 0; $j < $pad_zeros; $j++)
+            $to_add = "0" . $to_add;
+
+        // Add the ascii value to the total ascii string
+        $ascii_str .= $to_add;
+
+        // Add delimiter
         if($i != $len - 1)
             $ascii_str .= '-';
     }
